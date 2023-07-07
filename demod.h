d8132687d0fae99e578b6fd8fd7f7211bd224019
Ana <nasti09112002@gmail.com>
2023-07-07 08:54:57 -0300

    Правильная АМ

--- demod.h
+++ demod.h
@@ -15,41 +15,40 @@ public:
      * @return dftuygubni
      */
 
-    template<typename Type>
-    std::vector <Type> demodAM (SignalFile<Type> const & sigFile);
-    template<typename Type>
-    std::vector<Type> demodFM(SignalFile<Type> const & sigFile);
+    template < typename Type >
+    std::vector< Type > demodAM( SignalFile< Type > const& sigFile );
+    template < typename Type >
+    std::vector< Type > demodFM( SignalFile< Type > const& sigFile );
 private:
 };
 
-template<typename Type>
-std::vector <Type> Demodulation::demodAM (SignalFile<Type> const & sigFile){
+template < typename Type >
+std::vector< Type > Demodulation::demodAM( SignalFile< Type > const& sigFile ) {
     uint32_t size = sigFile.size();
-    std::vector <Type> data (size);
-    for(uint32_t i = 0; i < size; i++)
-    {
-        data[i] = sigFile.data(i).abs();
+    std::vector< Type > data( size );
+    for( uint32_t i = 0; i < size; i++ ) {
+        data[ i ] = sigFile.data( i ).abs() - 1;
     }
-    return std::move(data);
+    return std::move( data );
 }
 
-template<typename Type>
-std::vector<Type> Demodulation::demodFM (SignalFile<Type> const & sigFile){
+template < typename Type >
+std::vector< Type > Demodulation::demodFM( SignalFile< Type > const& sigFile ) {
     uint32_t size = sigFile.size();
     int64_t fCen = sigFile.freqCenter();
     int64_t fSam = sigFile.freqSample();
-    std::vector<Type> data(size);
-    for(uint32_t i = 0; i < size; i++){
-        data[i]=sigFile.data(i).arg();
+    std::vector< Type > data( size );
+    for( uint32_t i = 0; i < size; i++ ) {
+        data[ i ] = sigFile.data( i ).arg();
     }
-    for(uint32_t i = 0; i < size-1; i++){
-        Type dif = data[i+1]-data[i];
-        if(dif > M_PI) dif-=2*M_PI;
-        if(dif < -M_PI) dif+=2*M_PI;
-        data[i]=dif*fSam-fCen*2*M_PI;
+    for( uint32_t i = 0; i < size - 1; i++ ) {
+        Type dif = data[ i + 1 ] - data[ i ];
+        if( dif > M_PI ) dif -= 2 * M_PI;
+        if( dif < -M_PI ) dif += 2 * M_PI;
+        data[ i ] = dif * fSam - fCen * 2 * M_PI;
     }
-    data[size-1]=0;
-    return std::move(data);
+    data[ size - 1 ] = 0;
+    return std::move( data );
 }
 
 #endif // DEMOD_H
