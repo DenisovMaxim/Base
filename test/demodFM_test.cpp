53f3fd57613646f577108457e189886bbee0aa80
DenisovMaxim <maksdenis3@mail.ru>
2023-07-07 17:09:23 +0300

    add tests for FM demodulator & writer

--- test/demodFM_test.cpp
+++ test/demodFM_test.cpp
@@ -1,7 +1,26 @@
 #include <gtest/gtest.h>
+#include "../reader.h"
 #include "../demod.h"
+#include <vector>
+#include <fstream>
 
 TEST( TestDemodFM, Creating ) {
-	Demodulation testedDemObject;
+	Demodulation demod;
+}
 
+TEST( TestDemodFM, DemodFine ) {
+	Reader fileFM;
+	SignalFile< double > fileM = fileFM.readFile< double >( "data/testDataFM" );
+	Demodulation demod;
+	std::vector< double > data_ = demod.demodFM( fileM );  // vector
+	std::ifstream file( "data/testDataDM", std::fstream::binary );
+	uint64_t size = fileM.size();
+	std::vector< double > data( size );
+	for( uint64_t i = 0; i < size; i++ ) {
+		file.read( reinterpret_cast< char* >( &data[ i ] ), sizeof( double ) );
+	}
+	file.close();
+	for( uint64_t i = 0; i < size; i++ ) {
+		ASSERT_NEAR( data_[ i ], data[ i ], 1e-5 );
+	}
 }
